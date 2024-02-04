47158667e90e2694c560daa79948404abf7e5121
Cédric Honorat <chonorat@student.42lyon.fr>
2024-02-04 17:47:41 +0100

    Bad alloc exception caught in main

--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:19:21 by chonorat          #+#    #+#             */
-/*   Updated: 2024/02/04 16:36:58 by chonorat         ###   ########.fr       */
+/*   Updated: 2024/02/04 17:46:37 by chonorat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,28 @@
 
 int main()
 {
-	Data* storage = new Data();
-	uintptr_t cast = Serializer::serialize(storage);
-	Data* copy = Serializer::deserialize(cast);
+	try
+	{
+		Data* storage = new Data();
+		uintptr_t cast = Serializer::serialize(storage);
+		Data* copy = Serializer::deserialize(cast);
 
-	storage->setValue(42);
-	std::cout << "storage value: " << storage->getValue() << std::endl;
-	std::cout << "storage adress: " << storage << std::endl;
-	std::cout << "copy value: " << copy->getValue() << std::endl;
-	std::cout << "copy adress: " << copy << std::endl;
-	std::cout << std::endl;
-	std::cout << "copy value set to 21" << std::endl;
-	copy->setValue(21);
-	std::cout << "copy value: " << copy->getValue() << std::endl;
-	std::cout << "storage value: " << storage->getValue() << std::endl;
-	delete storage;
+		storage->setValue(42);
+		std::cout << "storage value: " << storage->getValue() << std::endl;
+		std::cout << "storage adress: " << storage << std::endl;
+		std::cout << "copy value: " << copy->getValue() << std::endl;
+		std::cout << "copy adress: " << copy << std::endl;
+		std::cout << std::endl;
+		std::cout << "copy value set to 21" << std::endl;
+		copy->setValue(21);
+		std::cout << "copy value: " << copy->getValue() << std::endl;
+		std::cout << "storage value: " << storage->getValue() << std::endl;
+		delete storage;
+	}
+	catch (std::bad_alloc &e)
+	{
+		std::cout << e.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
