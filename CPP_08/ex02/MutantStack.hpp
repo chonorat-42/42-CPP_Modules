bcea95e20d162de6412c135b70418656c7b80c5f
chonorat <chonorat@student.42lyon.fr>
2024-02-07 19:04:10 +0100

    CPP08 ex02 temp

---
+++ CPP_08/ex02/MutantStack.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 18:04:39 by chonorat          #+#    #+#             */
+/*   Updated: 2024/02/07 18:59:04 by chonorat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <iterator>
+
+class Iterator;
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack();
+		MutantStack(const MutantStack& other);
+		MutantStack& operator=(const MutantStack& other);
+		~MutantStack();
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator& begin();
+		iterator& end();
+};
+
+
+#endif //MUTANTSTACK_HPP
---
+++ CPP_08/ex02/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 18:04:27 by chonorat          #+#    #+#             */
+/*   Updated: 2024/02/07 19:01:23 by chonorat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <map>
+
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> temp;
+
+	MutantStack<int>::iterator it;
+	it = temp.begin();
+}
