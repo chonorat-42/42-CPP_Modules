6397853d35ce2e87a26d7d2880b5f05377807f14
chonorat <chonorat@student.42lyon.fr>
2024-02-08 15:46:16 +0100

    CPP08 ex02 done

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -6,31 +6,57 @@
 /*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 18:04:39 by chonorat          #+#    #+#             */
-/*   Updated: 2024/02/07 18:59:04 by chonorat         ###   ########.fr       */
+/*   Updated: 2024/02/08 15:40:10 by chonorat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-#include <iostream>
-#include <stack>
-#include <iterator>
-
-class Iterator;
+# include <stack>
 
 template<typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
 		MutantStack();
+		~MutantStack();
 		MutantStack(const MutantStack& other);
 		MutantStack& operator=(const MutantStack& other);
-		~MutantStack();
 		typedef typename std::stack<T>::container_type::iterator iterator;
-		iterator& begin();
-		iterator& end();
+		iterator begin();
+		iterator end();
 };
 
+template<typename T>
+MutantStack<T>::MutantStack() {}
+
+template<typename T>
+MutantStack<T>::MutantStack(const MutantStack &other)
+{
+	*this = other;
+}
+
+template<typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &other)
+{
+	this->c = other.c;
+	return (*this);
+}
+
+template<typename T>
+MutantStack<T>::~MutantStack() {}
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return (this->c.begin());
+}
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return (this->c.end());
+}
 
 #endif //MUTANTSTACK_HPP
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,18 +6,68 @@
 /*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 18:04:27 by chonorat          #+#    #+#             */
-/*   Updated: 2024/02/07 19:01:23 by chonorat         ###   ########.fr       */
+/*   Updated: 2024/02/08 15:43:52 by chonorat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <map>
-
 #include "MutantStack.hpp"
+#include <iostream>
 
 int main()
 {
-	MutantStack<int> temp;
+	MutantStack<int>	mstack;
 
-	MutantStack<int>::iterator it;
-	it = temp.begin();
+	std::cout << "mstack.push(5)" << std::endl;
+	mstack.push(5);
+	std::cout << "mstack.push(17)" << std::endl;
+	mstack.push(17);
+	std::cout << std::endl;
+
+	std::cout << "mstack.top(): " << mstack.top() << std::endl;
+
+	std::cout << "mstack.pop()" << std::endl << std::endl;
+	mstack.pop();
+
+	std::cout << "mstack.size(): " << mstack.size() << std::endl << std::endl;
+
+	std::cout << "mstack.push(3)" << std::endl;
+	mstack.push(3);
+	std::cout << "mstack.push(5)" << std::endl;
+	mstack.push(5);
+	std::cout << "mstack.push(737)" << std::endl;
+	mstack.push(737);
+	std::cout << "mstack.push(0)" << std::endl;
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	std::cout << std::endl;
+	while (it != ite)
+	{
+		std::cout << "*it = " << *it << std::endl;
+		++it;
+	}
+
+	std::cout << std::endl;
+	std::cout << "std::stack<int> s(mstack)" << std::endl;
+	std::stack<int> s(mstack);
+	std::cout << "s.size(): " << s.size() << std::endl;
+	std::cout << std::endl;
+	std::cout << "s.push(42)" << std::endl;
+	s.push(42);
+	std::cout << "s.size(): " << s.size() << std::endl;
+	std::cout << "mstack.size(): " << mstack.size() << std::endl;
+	std::cout << "s.top(): " << s.top() << std::endl;
+	std::cout << "mstack.top(): " << mstack.top() << std::endl;
+	std::cout << "it = mstack.begin()" << std::endl;
+	it = mstack.begin();
+	while (it != ite)
+	{
+		std::cout << "*it = " << *it << std::endl;
+		++it;
+	}
+	return (0);
 }
