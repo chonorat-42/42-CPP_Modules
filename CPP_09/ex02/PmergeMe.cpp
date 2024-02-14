ea4ec1caec82e4db8233802b132199551d8f878f
chonorat <chonorat@student.42lyon.fr>
2024-02-14 16:39:59 +0100

    Merge sort implemented

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -6,14 +6,18 @@
 /*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 13:31:17 by chonorat          #+#    #+#             */
-/*   Updated: 2024/02/12 15:53:30 by chonorat         ###   ########.fr       */
+/*   Updated: 2024/02/14 16:35:49 by chonorat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
 PmergeMe::PmergeMe()
-{}
+{
+	this->_vector.clear();
+	this->_vectorPair.clear();
+	this->_strayInt = -1;
+}
 
 PmergeMe::PmergeMe(const PmergeMe &other)
 {
@@ -27,7 +31,10 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other)
 }
 
 PmergeMe::~PmergeMe()
-{}
+{
+	this->_vector.clear();
+	this->_vectorPair.clear();
+}
 
 static bool validInteger(std::string arg)
 {
@@ -51,10 +58,95 @@ void PmergeMe::storeInVector(char **list)
 	}
 }
 
+void PmergeMe::storeInVectorPair()
+{
+	for (std::vector<int>::iterator it = this->_vector.begin(); it != this->_vector.end(); it += 2)
+	{
+		if (it + 1 == this->_vector.end())
+		{
+			this->_strayInt = *it;
+			break;
+		}
+		else
+			this->_vectorPair.push_back(std::make_pair(*it, *(it + 1)));
+	}
+	this->_vector.clear();
+}
+
+void PmergeMe::sortPairVector()
+{
+	for (std::vector< std::pair<int, int> >::iterator it = this->_vectorPair.begin(); it != this->_vectorPair.end(); it++)
+	{
+		if (it->first > it->second)
+			std::swap(it->first, it->second);
+	}
+}
+
+std::vector< std::pair<int, int> > PmergeMe::merge(std::vector< std::pair<int, int> > left, std::vector< std::pair<int, int> > right)
+{
+	std::vector< std::pair<int, int> > result;
+
+	while (!left.empty() && !right.empty())
+	{
+		if (left.front().second <= right.front().second)
+		{
+			result.push_back(left.front());
+			left.erase(left.begin());
+		}
+		else
+		{
+			result.push_back(right.front());
+			right.erase(right.begin());
+		}
+	}
+	while (!left.empty())
+	{
+		result.push_back(left.front());
+		left.erase(left.begin());
+	}
+	while (!right.empty())
+	{
+		result.push_back(right.front());
+		right.erase(right.begin());
+	}
+	return (result);
+}
+
+std::vector< std::pair<int, int> > PmergeMe::mergeSort(std::vector<std::pair<int, int> > vectorPair)
+{
+	if (vectorPair.size() < 2)
+		return (vectorPair);
+	std::vector< std::pair<int, int> > left;
+	std::vector< std::pair<int, int> > right;
+	for (size_t index = 0; index < vectorPair.size(); index++)
+	{
+		if (!(index % 2))
+			left.push_back(vectorPair[index]);
+		else
+			right.push_back(vectorPair[index]);
+	}
+	left = mergeSort(left);
+	right = mergeSort(right);
+	return (merge(left, right));
+}
+
+void PmergeMe::fillIntoContainer()
+{
+	this->_vector.push_back(this->_vectorPair.front().first);
+	for (std::vector< std::pair<int, int> >::iterator it = this->_vectorPair.begin(); it != this->_vectorPair.end(); it++)
+		this->_vector.push_back(it->second);
+
+}
+
 void PmergeMe::sortInVector(char **list)
 {
 	storeInVector(list);
-	this->_vec
+	if (this->_vector.empty())
+		throw ListEmpty();
+	storeInVectorPair();
+	sortPairVector();
+	this->_vectorPair = mergeSort(this->_vectorPair);
+	fillIntoContainer();
 }
 
 const char *PmergeMe::ListEmpty::what()const throw()
--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 12:58:43 by chonorat          #+#    #+#             */
-/*   Updated: 2024/02/12 15:55:20 by chonorat         ###   ########.fr       */
+/*   Updated: 2024/02/14 16:34:02 by chonorat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,15 @@ class PmergeMe
 		void sortInVector(char **list);
 
 	private:
+		int _strayInt;
 		std::vector<int> _vector;
+		std::vector< std::pair<int, int> > _vectorPair;
 		void storeInVector(char **list);
+		void storeInVectorPair();
+		void sortPairVector();
+		std::vector< std::pair<int, int> > mergeSort(std::vector< std::pair<int, int> > vectorPair);
+		std::vector< std::pair<int, int> > merge(std::vector< std::pair<int, int> > left, std::vector< std::pair<int, int> > right);
+		void fillIntoContainer();
 		PmergeMe(const PmergeMe& other);
 		PmergeMe& operator=(const PmergeMe& other);
 		class InvalidInteger : public std::exception
